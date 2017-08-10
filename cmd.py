ad33e8d2521743db79a60e3b61a64ef19d61ddf7
lijialinneu <1040591521@qq.com>
2017-08-10 15:12:09 +0800

    auto push at 2017-08-10 15:12:09.294485

--- cmd.py
+++ cmd.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 
 import subprocess
+import datetime
+
 
 subprocess.call(["git", "add", "."])
-subprocess.call(["git", "commit", "-m", "auto push"])
+subprocess.call(["git", "commit", "-m", "auto push at " + str(datetime.datetime.now())])
 subprocess.call(["git", "push"])
-subprocess.call(["lijialinneu"])
-subprocess.call(["lijialin19930715"])
