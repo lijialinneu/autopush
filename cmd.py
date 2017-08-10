8a2f604a0a07fdab083f7f50d149e8389b0f9d30
lijialinneu <1040591521@qq.com>
2017-08-10 14:54:27 +0800

    auto push

--- cmd.py
+++ cmd.py
@@ -5,3 +5,5 @@ import subprocess
 subprocess.call(["git", "add", "."])
 subprocess.call(["git", "commit", "-m", "auto push"])
 subprocess.call(["git", "push"])
+subprocess.call(["lijialinneu"])
+subprocess.call(["lijialin19930715"])
